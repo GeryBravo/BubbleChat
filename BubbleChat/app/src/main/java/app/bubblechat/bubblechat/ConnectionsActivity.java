166d30a02ec85121e33f9278c51b253ad1b216fe
gerybravo <petergergo2.0@gmail.com>
2016-01-08 23:14:42 +0100

    AddFriendFragment.java, ConnectionsFragment.java, UserProfileFragment.java added. fragment_connections.xml started.

--- BubbleChat/app/src/main/java/app/bubblechat/bubblechat/ConnectionsActivity.java
+++ BubbleChat/app/src/main/java/app/bubblechat/bubblechat/ConnectionsActivity.java
@@ -1,13 +1,27 @@
 package app.bubblechat.bubblechat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-public class ConnectionsActivity extends AppCompatActivity {
+import app.bubblechat.bubblechat.fragments.ConnectionsFragment;
+
+public class ConnectionsActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_connections);
+        setContentView(R.layout.connections_container);
+
+        if(findViewById(R.id.fragment_container)!=null)
+        {
+            if(savedInstanceState!=null)
+                return;
+
+            ConnectionsFragment firstFragment = new ConnectionsFragment();
+            firstFragment.setArguments(getIntent().getExtras());
+            getFragmentManager().beginTransaction().add(R.id.fragment_container, firstFragment).commit();
+        }
+
+
     }
 }
---
+++ BubbleChat/app/src/main/java/app/bubblechat/bubblechat/fragments/ConnectionsFragment.java
@@ -0,0 +1,32 @@
+package app.bubblechat.bubblechat.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import app.bubblechat.bubblechat.R;
+
+/**
+ * Created by gerybravo on 2016.01.08..
+ */
+public class ConnectionsFragment extends Fragment {
+
+    private RecyclerView connectionsList;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager manager;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance)
+    {
+        View v = inflater.inflate(R.layout.fragment_connections, container, false);
+
+        connectionsList = (RecyclerView) v.findViewById(R.id.rvConnectionsList);
+        manager = new LinearLayoutManager(getActivity());
+
+        return v;
+    }
+}
