4c57a349d608cc5eefa93a596f4ca2136a89539e
Erdei Benjámin <erdei_benjamin@hotmail.com>
2016-01-12 12:28:29 +0100

    Messaging

--- BubbleChat/app/src/main/java/app/bubblechat/bubblechat/fragments/ChatFragment.java
+++ BubbleChat/app/src/main/java/app/bubblechat/bubblechat/fragments/ChatFragment.java
@@ -2,18 +2,174 @@ package app.bubblechat.bubblechat.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import app.bubblechat.bubblechat.objects.ChatListAdapter;
+import app.bubblechat.bubblechat.objects.Message;
 import app.bubblechat.bubblechat.R;
 
 /**
  * Created by Ben on 2016.01.10..
  */
 public class ChatFragment extends Fragment {
+
+    private Handler handler = new Handler();
+
+    private static final String USER_ID_KEY = "userId";
+    private Button bSend;
+    private EditText tMessage;
+    private EditText tAdress;
+    private String message;
+    private static String sUserId;
+
+    private ListView lvChat;
+    private ArrayList<Message> mMessages;
+    private ChatListAdapter mAdapter;
+    // Keep track of initial load to scroll to the bottom of the ListView
+    private boolean mFirstLoad;
+
+
+    private static final int MAX_CHAT_MESSAGES_TO_SHOW = 50;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance) {
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+
+        ParseObject.registerSubclass(Message.class);
+        View v = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        if (ParseUser.getCurrentUser() != null) { // start with existing user
+            sUserId = ParseUser.getCurrentUser().getObjectId();
+            setupMessagePosting(v);
+        } else {
+            //visszanavigálunk a login oldalra
+        }
+
+        handler.postDelayed(runnable, 100);
+
+/*
+
+        bSend = (Button) v.findViewById(R.id.bSend);
+        tMessage = (EditText) v.findViewById(R.id.tMessage);
+
+
+        bSend.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                message = tMessage.getText().toString();
+
+                String data = tMessage.getText().toString();
+                ParseObject message = ParseObject.create("Message");
+                message.put(USER_ID_KEY, sUserId);
+                message.put("body", data);
+                message.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        Toast.makeText(getActivity(), "Successfully created message on Parse",
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+                tMessage.setText("");
+            }
+        });
+*/
+        return v;
+    }
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            refreshMessages();
+            handler.postDelayed(this, 100);
+        }
+    };
+    private void refreshMessages() {
+        receiveMessage();
+    }
+
+    private void setupMessagePosting(View v) {
+        tMessage = (EditText) v.findViewById(R.id.tMessage);
+        tAdress = (EditText) v.findViewById(R.id.tAdress);
+        bSend = (Button) v.findViewById(R.id.bSend);
+        lvChat = (ListView) v.findViewById(R.id.lvChat);
+        mMessages = new ArrayList<Message>();
+        // Automatically scroll to the bottom when a data set change notification is received and only if the last item is already visible on screen. Don't scroll to the bottom otherwise.
+        lvChat.setTranscriptMode(1);
+        mFirstLoad = true;
+        mAdapter = new ChatListAdapter(getActivity(), sUserId, mMessages);
+        lvChat.setAdapter(mAdapter);
+        bSend.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String body = tMessage.getText().toString();
+                String adress = tAdress.getText().toString();
+                // Use Message model to create new messages now
+                Message message = new Message();
+                message.setUserId(sUserId);
+                message.setAdressId(adress);
+                message.setBody(body);
+                message.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        receiveMessage();
+                    }
+                });
+                tMessage.setText("");
+            }
+        });
+    }
+
+    private void receiveMessage() {
+
+        // Construct query to execute
+        ParseQuery<Message> query = ParseQuery.getQuery(Message.class);
+        // Configure limit and sort order
+        query.setLimit(MAX_CHAT_MESSAGES_TO_SHOW);
+        query.orderByAscending("createdAt");
+        // Execute query to fetch all messages from Parse asynchronously
+        // This is equivalent to a SELECT query with SQL
+        query.findInBackground(new FindCallback<Message>() {
+            public void done(List<Message> messages, ParseException e) {
+                if (e == null) {
+                    mMessages.clear();
+                    Log.d("Messages:", mMessages.toString());
+
+                    for (Message m : messages
+                            ) {
+                        if(m.getAdressId().equals(sUserId))
+                        {
+                            mMessages.add(m);
+                        }
+                    }
+                    mAdapter.notifyDataSetChanged(); // update adapter
+                    // Scroll to the bottom of the list on initial load
+                    if (mFirstLoad) {
+                        lvChat.setSelection(mAdapter.getCount() - 1);
+                        mFirstLoad = false;
+                    }
+                } else {
+                    Log.d("message", "Error: " + e.getMessage());
+                }
+            }
+        });
+
     }
 }
---
+++ BubbleChat/app/src/main/java/app/bubblechat/bubblechat/objects/Message.java
@@ -0,0 +1,36 @@
+package app.bubblechat.bubblechat.objects;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+/**
+ * Created by Ben on 2016.01.11..
+ */
+
+
+
+@ParseClassName("Message")
+public class Message extends ParseObject {
+    public String getUserId() {
+        return getString("userId");
+    }
+
+    public String getAdressId() {
+        return getString("adressId");
+    }
+
+    public String getBody() {
+        return getString("body");
+    }
+
+    public void setUserId(String userId) {
+        put("userId", userId);
+    }
+    public void setAdressId(String adressId) {
+        put("adressId", adressId);
+    }
+    public void setBody(String body) {
+        put("body", body);
+    }
+
+}
